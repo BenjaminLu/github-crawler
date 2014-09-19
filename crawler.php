c5f741c900e116c264c935e14b7ddb46438fd3f9
BenjaminLu <coopermilk123@gmail.com>
2014-09-19 16:49:23 +0800

    Feature : browse all popular repositories and fetch all of pages.

--- crawler.php
+++ crawler.php
@@ -1,42 +1,88 @@
 <?php
+fetchPopularRepositories(3000);
+$filepath = "urls/out.txt";
+readFileAndFetchEachRepo($filepath, "repo_","_src");
 
-for ($i = 60; $i <= 100; $i++) { 
-	$url = "https://github.com/search";
-	$params = array(
-	  "q"   	=>	"stars:>3000",
-	  "utf8"	=>	"%E2%9C%93",
-	  "type"	=>	"Repositories",
-	  "ref" 	=>	"searchresults",
-	  "p"		=>  $i
-	);
+function readFileAndFetchEachRepo($filename = '', $filePrefix, $fileSuffix)
+{
+	$source_file = fopen( $filename, "r" ) or die("Couldn't open $filename");
 
-	$url .= '?' . http_build_query($params);
+   	$file = fopen($filename, "r");
+   	$i = 1;
+	while (!feof($file)) {
+		$targetURL = fgets($file);
+		$targetURL = str_replace("\r\n",'', $targetURL);
 
-	$header = get_web_page($url);
-
-	if(isset($header['errno'])) {
-		echo "Error :" . $header['errno'] . "\r\n";
-	}
-	//echo $header['content'] . "\n" . "Page" . $i;
-	preg_match_all("/<h3 class=\"repo-list-name\">\s*<a href=\"\/(.*)\">(.*)<\/a>/U", $header['content'], $output);
-	
-	$matches = $output[0];
-	for ($j=0; $j < count($matches); $j++) { 
-		if(!isset($matches[$j])) {
-			exit(0);
+		if($targetURL == "\r\n" || $targetURL == "\n" || $targetURL == "" || $targetURL == null) {
+			echo "Job finished";
+			exit(1);
 		}
-		$pureURL = getPureURL($matches[$j]);
-		$aURL = "https://github.com". $pureURL . "\r\n";
-		echo $aURL;
-		$outFile = "out.txt";
-		$fh = fopen($outFile, 'a+'); 
-		fwrite($fh,$aURL);
-    	fclose($fh);
 
+		$pureRepoName = getRepoName($targetURL);
+
+		printf("%-75s", "Fetching " . $targetURL . " ... ");
+		$html = get_web_page($targetURL);
+		$outFile = "resource/" . "repo_".  $pureRepoName  . ".txt";
+		$fh = fopen($outFile, 'a+'); 
+		fwrite($fh,$html['content']);
+		fclose($fh);
+		printf("%-5s", "done" . "\r\n");
+		$i++;
 	}
 
-	sleep(30);
+	fclose($file);
+}
 
+function fetchPopularRepositories($starNumber) 
+{
+	for ($i = 60; $i <= 100; $i++) { 
+		$url = "https://github.com/search";
+		$params = array(
+		  "q"   	=>	"stars:>" + $starNumber,
+		  "utf8"	=>	"%E2%9C%93",
+		  "type"	=>	"Repositories",
+		  "ref" 	=>	"searchresults",
+		  "p"		=>  $i
+		);
+
+		$url .= '?' . http_build_query($params);
+
+		echo "Fetching All Repositories which have over ".$starNumber." stars\r\n";
+		$header = get_web_page($url);
+
+		if(isset($header['errno'])) {
+			echo "Error :" . $header['errno'] . "\r\n";
+		}
+		//echo $header['content'] . "\n" . "Page" . $i;
+		preg_match_all("/<h3 class=\"repo-list-name\">\s*<a href=\"\/(.*)\">(.*)<\/a>/U", $header['content'], $output);
+		
+		$matches = $output[0];
+		for ($j=0; $j < count($matches); $j++) { 
+
+			if(!isset($matches[$j])) {
+				exit(0);
+			}
+
+			$pureURL = getPureURL($matches[$j]);
+			$aURL = "https://github.com". $pureURL . "\r\n";
+			echo $aURL;
+			$outFile = "urls/out.txt";
+			$fh = fopen($outFile, 'a+'); 
+			fwrite($fh,$aURL);
+			fclose($fh);
+
+		}
+
+		sleep(30);
+	}
+}
+
+function getRepoName($targetURL)
+{
+	$targetURL = substr($targetURL, 18);
+	$targetURL = str_replace("//","_",$targetURL);
+	$targetURL = str_replace("/","_",$targetURL);
+	return $targetURL;
 }
 
 function getPureURL($htmlSnippet)
